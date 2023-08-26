c9ee98c8beb937a11074da536d2106fb5f2f0c63
Uniquegbenga <fagbenrogbenga1@gmail.com>
2023-08-26 03:49:12 -0700

    Task 6 done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,40 +1,16 @@
 #include <stdio.h>
 /**
-* main - a program that single print all no
-* Return: Always 0
+ * main - a program that single print all no
+ * Return: Always 0 (Success)
 */
-
 int main(void)
 {
-putchar('0');
-putchar('\n');
+int c;
+for (c = 0; c < 10; c++)
 
-putchar('1');
+{
+putchar(c);
+}
 putchar('\n');
-
-putchar('2');
-putchar('\n');
-
-putchar('3');
-putchar('\n');
-
-putchar('4');
-putchar('\n');
-
-putchar('5');
-putchar('\n');
-
-putchar('6');
-putchar('\n');
-
-putchar('7');
-putchar('\n');
-
-putchar('8');
-putchar('\n');
-
-putchar('9');
-putchar('\n');
-
 return (0);
 }
