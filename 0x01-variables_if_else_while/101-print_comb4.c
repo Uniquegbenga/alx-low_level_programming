6307eac4028662b285b7112d3b68e3a8909b77d2
Uniquegbenga <fagbenrogbenga1@gmail.com>
2023-08-26 05:22:22 -0700

    Task 101 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all possible different combinations
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num1, num2, num3;
+for (num1 = 0; num1 <= 7; num1++)
+{
+for (num2 = 0; num2 <= 8; num2++)
+{
+for (num3 = 0; num3 <= 9; num3++)
+{
+putchar((num1 % 10) + '0');
+putchar((num2 % 10) + '0');
+putchar((num3 % 10) + '0');
+
+if (num1 == 7 && num2 == 8 && num3 == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
